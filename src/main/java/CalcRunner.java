85bc4322cc85323f5bf46997e78a7db096fa32af
Aleks <no>
2021-05-06 18:04:13 +0300

    homework 3

--- src/main/java/CalcRunner.java
+++ src/main/java/CalcRunner.java
@@ -2,23 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CalcRunner {
-   static ArrayList<Object> result = new ArrayList<>();
+    static ArrayList<Object> result = new ArrayList<>();
     static int count2 = 0;
     static int numberOne;
     static int numberTwo;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
-
-
-
-
-        while (count == 0 &&  count2 <= 5 ) {
-
+        while (count == 0 && count2 <= 5) {
             System.out.println("Введите код операции (0 - сумма, 1 - разность, 2 - произведение, 3 - деление, 4 - закончить работу, 5 - введите номер операции чтобы узнать результат");
             int numb = scanner.nextInt();
-
-
             switch (numb) {
                 case 0:
                     System.out.println("Введите первое число");
@@ -46,6 +40,14 @@ public class CalcRunner {
                     numberOne = scanner.nextInt();
                     System.out.println("Введите второе число");
                     numberTwo = scanner.nextInt();
+                    while (true) {
+                        if (numberTwo == 0 ) {
+                            System.out.println("вы ввели ноль");
+                            numberTwo = scanner.nextInt();
+                        }else {
+                            break;
+                        }
+                    }
                     System.out.println(delenie(numberOne, numberTwo));
                     break;
                 case 4:
@@ -62,37 +64,37 @@ public class CalcRunner {
             }
 
 
-
         }
         if (count2 == 5) {
             System.out.println("калькулятор перегрелся.");
         }
 
     }
+
     public static int sum(int a, int b) {
         count2++;
         result.add(a + b);
         return a + b;
     }
+
     public static int raz(int a, int b) {
         count2++;
         result.add(a - b);
         return a - b;
     }
+
     public static int proizved(int a, int b) {
         count2++;
         result.add(a * b);
         return a * b;
     }
+
     public static double delenie(int a, int b) {
         double res = 0.00;
-        if (b != 0) {
-            res =  (double) (a / b);
-            result.add(res);
+            res =  (double) a / b;
+            result.add("операция деление, результат " + res);
             count2++;
-        }else {
-            System.out.println("Вы ввели ноль, на ноль делить нельзя.");
-        }
+
         return res;
     }
 
